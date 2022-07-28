3c71b3a9f036c54dc236a3b6d2b794fb7308f621
MakeShiftArtist <lrmcbride4@gmail.com>
2022-07-27 20:44:59 -0500

    Feat: Add account.ts to REST types

---
+++ src/rest/account.ts
@@ -0,0 +1,4 @@
+import { RESTAPISuccessResponse } from "../utils/rest";
+import { APIClientUser } from "../payloads/client";
+
+export type RESTAPIAccountSuccess = RESTAPISuccessResponse<APIClientUser>;
