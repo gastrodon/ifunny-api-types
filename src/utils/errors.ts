163bd0106ffb7c89a8e599ef62f31f3ab47f4ba6
MakeShiftArtist <lrmcbride4@gmail.com>
2022-08-02 16:00:54 -0500

    Feat: Add all known iFunny API Errors

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,17 +1,17 @@
 /**
  * Thrown when trying to login with invalid email or password
  */
-export interface RESTAPIInvalidCreds {
-	error: "invalid_grant";
-	error_description: "Wrong user credentials";
-	status: 400;
+export interface RESTAPIError {
+	error: RESTAPIiFunnyError;
+	error_description: string;
+	status: 400 | 401 | 403 | 404 | 500;
 }
 
 /**
  * This is thrown when you need to solve a captcha to complete the request.
  */
-export interface RESTAPICaptchaError {
-	error: "captcha_required";
+export interface RESTAPICaptchaError extends RESTAPIError {
+	error: IFUNNY_ERRORS.CAPTCHA_REQUIRED;
 	error_description: "Human verification is required";
 	data: {
 		/**
@@ -28,60 +28,44 @@ export interface RESTAPICaptchaError {
  */
 enum CaptchaTypes {
 	FUN_CAPTCHA = "fun_captcha",
+	RECAPTCHA = "recaptcha",
 }
 
 type CaptchaType = `${CaptchaTypes}`;
 
 /**
- * Returned when the authorization bearer token has expired or been revoked.
- */
-export interface RESTAPIExpiredBearerError {
-	status: 401;
-	error: "invalid_grant";
-	error_description: "token is expired";
-}
-
-/**
- * Returned when the authorization bearer token could not be parsed by the API.
- */
-export interface RESTAPICannotParseBearerError {
-	status: 401;
-	error: "invalid_grant";
-	error_description: `Cannot parse ${string}`;
-}
-
-/**
- * Returned when the authorization bearer token is invalid.
- */
-export interface RESTAPIInvalidBearerError {
-	status: 401;
-	error: "invalid_grant";
-	error_description: "invalid access token";
-}
-
-/**
- * Returned when the Basic token is invalid
- */
-export interface RESTAPIMalformedTokenError {
-	status: 401;
-	error: "invalid_client";
-	error_description: "Malformed token: underscore";
-}
-
-/**
- * Returned when the request is made with a basic token but required a bearer token.
- */
-export interface RESTAPIUnauthorizedError {
-	status: 401;
-	error: "unauthorized";
-	error_description: "Requested operation is unavailable for guests";
-}
-
-/**
  * Returns when the request throws an error.
  */
-export type RESTAPIErrorResponse =
-	| RESTAPIInvalidBearerError
-	| RESTAPIMalformedTokenError
-	| RESTAPIUnauthorizedError
-	| RESTAPICaptchaError;
+export type RESTAPIErrorResponse = RESTAPICaptchaError | RESTAPIError;
+
+export type RESTAPIiFunnyError = `${IFUNNY_ERRORS}`;
+
+export enum IFUNNY_ERRORS {
+	ALREADY_REPUBLISHED = "already_republished",
+	ALREADY_SMILED = "already_smiled",
+	ALREADY_UNSMILED = "already_unsmiled",
+	BAD_REQUEST = "bad_request",
+	CAPTCHA_REQUIRED = "captcha_required",
+	CONTENT_WAS_DELETED = "content_was_deleted",
+	EMAIL_NOT_CONFIRMED = "email_not_confirmed",
+	ENTITY_ABUSED = "entity_abused",
+	FORBIDDEN_FOR_BANNED = "forbidden_for_banned",
+	FORBIDDEN_FOR_DELETED = "forbidden_for_deactivated",
+	INVALID_CLIENT = "invalid_client",
+	INVALID_GRANT = "invalid_grant",
+	NOT_REPUBLISHED = "not_republished",
+	NOT_SMILED = "not_smiled",
+	NOT_UNSMILED = "not_unsmiled",
+	PINNED_LIMIT_EXCEEDED = "profile_pins_to_many_pins_error",
+	PUBLISHING_TIMEOUT = "publishing_timeout",
+	QUERY_TOO_SHORT = "query_too_short",
+	ROOT_COMMENT_WAS_DELETED = "root_comment_was_deleted",
+	SOURCE_IS_UNAVAILABLE = "source_is_unavailable",
+	STOP_WORD = "content_tag_stopword",
+	TOO_MANY_UPPERCASE = "too_many_uppercase",
+	UNACCEPTABLE_SYMBOLS = "unacceptable_symbols",
+	UNAUTHORIZED = "unauthorized",
+	UNKNOWN_SOURCE = "unknown_source",
+	VIDEO_IS_INVALID = "video_is_invalid",
+	YOU_ARE_BLOCKED = "you_are_blocked",
+}
