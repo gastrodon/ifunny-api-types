ad970ca06524234a1a47428b1bf5ed314dee64e7
MakeShiftArtist <lrmcbride4@gmail.com>
2022-08-09 10:40:32 -0500

    Fix: TokenTypes changed to standard enum case

--- src/rest/oauth2.ts
+++ src/rest/oauth2.ts
@@ -1,23 +1,24 @@
-import {
-	RESTAPICaptchaError,
-	RESTAPIErrorResponse,
-	RESTAPIInvalidCreds,
-} from "../utils/errors";
+import { RESTAPICaptchaError, RESTAPIErrorResponse } from "../utils/errors";
 
-export type RESTAPIOauth2Login =
-	| RESTAPIOauth2LoginSuccess
-	| RESTAPIOauth2LoginError
-	| RESTAPICaptchaError;
+export type RESTAPIOauth2Login = RESTAPIOauth2LoginSuccess | RESTAPIOauth2LoginError;
 
 export interface RESTAPIOauth2LoginSuccess {
+	/**
+	 * Bearer token used for requests
+	 */
 	access_token: string;
-	token_type: `bearer`;
+	token_type: TOKEN_TYPES.BEARER;
+	/**
+	 * ? This Timestamp is in seconds, which is equivilent to 10 years
+	 */
 	expires_in: 315360000;
 }
 
-export enum TokenTypes {
-	Bearer = "bearer",
-	Basic = "basic",
+export enum TOKEN_TYPES {
+	BEARER = "bearer",
+	BASIC = "basic",
 }
 
-export type RESTAPIOauth2LoginError = RESTAPIInvalidCreds | RESTAPIErrorResponse;
+export type APITokenType = `${TOKEN_TYPES}`;
+
+export type RESTAPIOauth2LoginError = RESTAPIErrorResponse | RESTAPICaptchaError;
