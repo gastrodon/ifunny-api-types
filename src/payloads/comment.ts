2f5774a111d2de94cc1c1b661568dd0f0bfffd6f
MakeShiftArtist <lrmcbride4@gmail.com>
2022-08-10 00:32:52 -0500

    Feat: Refactor payloads/comment.ts

--- src/payloads/comment.ts
+++ src/payloads/comment.ts
@@ -1,15 +1,15 @@
 import {
-	APIGifCaptionPost,
-	APIGifPost,
-	APIPost,
-	APIPostCreator,
-	APIPostThumbnail,
-} from "./post";
+	APIContent,
+	APIContentCreator,
+	APIContentGif,
+	APIContentThumbnail,
+} from "./content";
+import { APIUser, APIUserRating } from "./user";
 
-import { APIBasePayload } from "./base";
-import { APIUserRating } from "./user";
-
-export interface APICommentBase extends APIBasePayload {
+/**
+ * Represents a Comment from iFunny
+ */
+export interface APICommentBase {
 	id: string;
 	cid: string;
 	status: string;
@@ -21,48 +21,84 @@ export interface APICommentBase extends APIBasePayload {
 	is_unsmiled: boolean;
 	is_edited: boolean;
 	user: APICommentAuthor;
-}
-
-export interface APIComment {
-	content: APIPost;
+	content: APIContent;
 	attachments: APICommentAttachments;
-	content_thumbs: APIPostThumbnail;
-	last_reply?: APILastReply;
-	root_comm_id?: string;
-	parent_comm_id?: string;
-	depth?: number;
+	content_thumbs: APIContentThumbnail;
+	last_reply?: APIReply;
+	state?: APICommentState;
 }
 
-interface APILastReply {
+/**
+ * Represents a Reply to a Comment from iFunny
+ */
+export interface APIReply extends APICommentBase {
 	root_comm_id: string;
 	parent_comm_id: string;
 	depth: number;
-	attachments: APICommentAttachments;
-	content_thumbs: APIPostThumbnail;
 }
 
-interface APICommentAuthor extends APIPostCreator {
+/**
+ * Represents a Comment from iFunny
+ */
+export type APIComment = APICommentBase | APIReply;
+
+export interface APICommentNum {
+	smiles: number;
+	unsmiles: number;
+	replies: number;
+}
+
+/**
+ * All states of the comment
+ */
+export enum COMMENT_STATE {
+	NORMAL = "normal",
+	TOP_COMMENT = "top",
+	ABUSED = "abused",
+	DELETED = "deleted",
+	AUTHOR_DELETED = "deleted_self",
+}
+
+/**
+ * All possible states of the comment
+ */
+export type APICommentState = `${COMMENT_STATE}`;
+
+interface APICommentAuthor extends APIContentCreator {
 	rating: APIUserRating;
 }
 
 interface APICommentAttachments {
-	content: APIPost[];
-	content_from_links: APIPost[];
+	content: APIContent[];
+	content_from_links: APIContent[];
 	mention_user: APIMentionUser[];
-	giphy: (APIGifPost | APIGifCaptionPost)[];
+	giphy: APIContentGif[];
 }
 
-interface APIMentionUser {
+export type APICommentContent = Pick<
+	APIContent,
+	| "id"
+	| "type"
+	| "is_abused"
+	| "bg_color"
+	| "shot_status"
+	| "state"
+	| "size"
+	| "thumb"
+	| "source"
+	| "creator"
+>;
+
+export interface APIMentionUser {
+	/**
+	 * ID of the Mention\
+	 * ? Increments last digit of the comment id
+	 */
 	id: string;
-	user_id: string;
-	nick: string;
+	creator: APIContentCreator;
+	nick: APIUser["nick"];
 	start_index: number;
 	stop_index: number;
-	original_nick: string;
-}
-
-interface APICommentNum {
-	smiles: number;
-	unsmiles: number;
-	replies: number;
+	user_id: APIUser["id"];
+	original_nick: APIUser["original_nick"];
 }
