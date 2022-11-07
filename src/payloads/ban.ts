40ce5057c98bb67cffc288d0ff828a8b26a61485
makeshiftartist <lrmcbride4@gmail.com>
2022-11-06 18:30:32 -0600

    Feat: Add APIBan and APIBanSmall

--- src/payloads/ban.ts
+++ src/payloads/ban.ts
@@ -1,36 +1,68 @@
-import { Timestamp } from "../utils";
+import { EpochSec } from "../utils";
+import { APIComment } from "./comment";
+import { APIContent } from "./content";
 
 /**
  * Represents a user's ban from the API
  */
-export interface APIBan {
+export interface APIBanSmall {
 	/**
 	 * Unique ban Id
 	 */
 	id: string;
 	/**
-	 * Unix timestamp of when the ban expires\
+	 * Unix EpochSec of when the ban expires\
 	 * ? IN SECONDS! More better accuracy, convert to milliseconds
 	 */
-	date_until: Timestamp;
+	date_until: EpochSec;
 	/**
 	 * Known {@link BAN_TYPES types}
 	 */
 	type: APIBanReason;
 }
 
+export interface APIBan extends APIBanSmall {
+	ban_reason: APIBanReason;
+	created_at: EpochSec;
+	/**
+	 * ? Only observed to be 1, 3, 17
+	 */
+	pid: number;
+	is_appealed: boolean;
+	can_be_appealed: boolean;
+	was_shown: boolean;
+	is_active: boolean;
+	is_shortable: boolean;
+	related_content?: APIContent;
+	related_comment?: APIComment;
+	date_until_minimum?: EpochSec; // Not observed
+	/**
+	 * @example
+	 * "commenting was suspended from Aug 19, 2021 to Aug 26, 2021"
+	 * "content creation suspended until Sep 15, 2022"
+	 */
+	type_message?: string;
+	/**
+	 * @example
+	 * "Hate Speech"
+	 * "Death / gore"
+	 */
+	ban_reason_message?: string;
+}
+
 /**
  * All known ban reasons
  */
 export enum BAN_REASONS {
-	SPAM = "bot_spam",
 	ABUSE = "abuse_harassment",
 	CHILD_PORN = "child_pornography",
 	HARDCORE = "hardcore",
-	OTHER = "other",
-	DEATH_GORE = "death_gore",
 	HATE_SPEECH = "hate_speech",
+	SPAM = "bot_spam",
 	THREATS = "threats_of_harm",
+	// Not available in mod menu
+	DEATH_GORE = "death_gore",
+	OTHER = "other",
 }
 
 /**
@@ -42,17 +74,16 @@ export type APIBanReason = `${BAN_REASONS}`;
  * All known ban types
  */
 export enum BAN_TYPES {
-	CONTENT_CREATION = "content_creation",
+	// from mobi.ifunny.bans.BanType
 	CHAT_ACCESS = "chat_access",
-	SUBSCRIBING = "subscribing",
-	COMMENTING = "comment_creation",
+	COMMENT_CREATION = "comment_creation",
+	CONTENT_CREATION = "content_creation",
+	PROFILE_ACCESS = "profile_access",
 	REPUBING = "repubing",
-	PROFILE = "profile_access",
 	SMILING = "smiling",
+	SUBSCRIBING = "subscribing",
 	OTHER = "other",
-	/**
-	 * Not in app but from requests.
-	 */
+	// Observed
 	COLLECTIVE_SHADOW = "collective_shadow",
 }
 
